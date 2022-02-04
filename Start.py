ae9a7d70fc9ed619c77a81a7d8fef1846a4bf6c1
NetoGerardo <neto-1997@live.com.pt>
2022-02-04 11:18:45 -0300

    Update Start.py

--- Start.py
+++ Start.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-#USED TO ORDER PRODUCTS
+#USED TO SORT PRODUCTS
 from selenium.webdriver.support.ui import Select
 
 from selenium.webdriver.common.by import By
